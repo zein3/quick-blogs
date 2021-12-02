4a031d970cf96d139ac0ca137a4656f66d1b2a5f
zein3 <zeinhaddad02@gmail.com>
2021-12-02 10:56:26 +0700

    installed tailwindcss and alpinejs

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <title>Quick</title>
+    </head> 
+    <body>
+        @yield('content')
+    </body>
+</html>
