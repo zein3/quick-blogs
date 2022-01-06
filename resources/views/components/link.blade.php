c9e7e3c54b1a421d897935f87bac062a50063af4
Fakhirul Akmal <fakhirula27@gmail.com>
2022-01-06 20:01:48 +0700

    Upgrade tailwind

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,2 @@
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<script src="https://kit.fontawesome.com/b32963d457.js" crossorigin="anonymous"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,42 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <title>Quick</title>
+        <title>Quick Blogs</title>
+        <x-link/>
     </head> 
     <body>
         @yield('body')
+
+        <header class="w-10/12 mx-auto py-6">
+            <div class="flex justify-between">
+                <h1>Quick</h1>
+                <nav class="flex justify-between gap-x-10">
+                    <ul class="flex gap-x-5">
+                        <li><a class="" href="">Kampus</a></li>
+                        <li><a class="" href="">Jurusan</a></li>
+                        <li><a class="" href="">Karir</a></li>
+                        <li><a class="" href="">Mitra</a></li>
+                    </ul>
+                    <ul class="flex gap-x-2">
+                        <li><a class="px-4 py-3 border" href="">Daftar</a></li>
+                        <li><a class="px-4 py-3 bg-blue-500" href="">Masuk</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main></main>
+        <footer>
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </footer>
+
+        <x-script/>
     </body>
 </html>
