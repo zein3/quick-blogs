7817b7f529fb1ad321116e6521eeecac56de82d8
Fakhirul Akmal <fakhirula27@gmail.com>
2022-01-06 21:18:55 +0700

    Login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -26,7 +26,7 @@
                     <input id="email-address" name="email" type="text" value="{{ old('email') }}" autocomplete="email"
                         required
                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        placeholder="Email address">
+                        placeholder="Username or email">
                 </div>
                 <div>
                     <label for="password" class="sr-only">Password</label>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,24 +1,119 @@
 @extends('layouts.app')
 
 @section('body')
-    @if(session('error'))
-        <p>{{ session('error') }}</p>
-    @endif
+@if(session('error'))
+<p>{{ session('error') }}</p>
+@endif
 
-    @foreach($errors->all() as $message)
-        <p>{{ $message }}</p>
-    @endforeach
+@foreach($errors->all() as $message)
+<p>{{ $message }}</p>
+@endforeach
 
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
+<form class="mt-8 space-y-6" action="{{ route('register') }}" method="POST">
+    @csrf
 
-        <input name="username" type="text" placeholder="username" value="{{ old('username') }}">
-        <input name="full_name" type="text" placeholder="full name" value="{{ old('fullname') }}">
-        <input name="email" type="email" placeholder="email" value="{{ old('email') }}">
-        <input name="phone_number" type="text" placeholder="phone number" value="{{ old('phone') }}">
-        <input name="password" type="password" placeholder="password">
-        <input name="password_confirmation" type="password" placeholder="confirm password">
-
-        <button type="submit">Register</button>
-    </form>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-2xl w-full space-y-8 shadow-lg rounded-md">
+            <div class="pt-5">
+                <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                    Register and be useful to others
+                </h2>
+            </div>
+            <div class="px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="username">
+                            Username
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                            id="username" name="username" type="text" placeholder="Username"
+                            value="{{ old('username') }}">
+                        <p class="text-red text-xs italic">Please fill out this field.</p>
+                    </div>
+                    <div class="md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="full_name">
+                            Full Name
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                            id="full_name" name="full_name" type="text" placeholder="Full Name"
+                            value="{{ old('fullname') }}">
+                    </div>
+                </div>
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-full px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="email">
+                            Email Address
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
+                            id="email" type="email" placeholder="example@example.com" value="{{ old('email') }}">
+                    </div>
+                </div>
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-full px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="phone_number">
+                            Phone Number
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
+                            id="phone_number" type="tel" placeholder="62 812 3456 7890" value="{{ old('phone') }}">
+                    </div>
+                </div>
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="password">
+                            Password
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                            id="password" name="password" type="password" placeholder="**********">
+                        <p class="text-red text-xs italic">Make it as long and as crazy as you'd like</p>
+                    </div>
+                    <div class="md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="password_confirmation">
+                            Password Confirmation
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                            id="password_confirmation" name="password_confirmation" type="password"
+                            placeholder="**********">
+                    </div>
+                </div>
+                <div class="flex items-center justify-between my-5">
+                    <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox"
+                            class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                            Saya setuju dengan segala kebijakan Quick.
+                        </label>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit"
+                        class="group relative w-full flex justify-center p-3 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                            <!-- Heroicon name: solid/lock-closed -->
+                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 @endsection
