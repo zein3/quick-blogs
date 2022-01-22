993389024afc8d6f51496ecb75609ec8d1b087b1
Fakhirul Akmal <fakhirula27@gmail.com>
2022-01-22 23:28:57 +0700

    edit index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             </div>
         @endif
 
-        <header class="w-10/12 mx-auto text-gray-600 body-font border-b">
+        <header class="w-11/12 mx-auto text-gray-600 body-font border-b">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
@@ -59,13 +59,13 @@
                 @endauth
             </div>
         </header>
-        <main class="container md:w-10/12 my-5 mx-auto">
+        <main class="container md:w-11/12 my-5 mx-auto">
 
             @yield('body')
 
         </main>
         <footer class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto">
+            <div class="w-11/12 px-5 py-24 mx-auto">
                 <div class="flex flex-wrap md:text-left text-center order-first">
                     <div class="lg:w-1/4 md:w-1/2 w-full px-4">
                         <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
@@ -123,9 +123,7 @@
                         <div
                             class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
                             <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-                                <label for="footer-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
-                                <input type="text" id="footer-field" name="footer-field"
-                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input type="text" id="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                             <button
                                 class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Button</button>
@@ -137,7 +135,7 @@
                 </div>
             </div>
             <div class="bg-gray-100">
-                <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+                <div class="container px-4 py-6 mx-auto flex items-center sm:flex-row flex-col">
                     <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2022 Quick — All rights reserved.
                     </p>
                     <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
