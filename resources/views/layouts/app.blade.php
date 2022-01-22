a13c85abe93ff27594f6f4921c3d67aa2218b4ef
Fakhirul Akmal <fakhirula27@gmail.com>
2022-01-22 17:49:11 +0700

    edit index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,102 +52,158 @@
                     @endauth
                 </nav>
             </div>
+            <header class="text-gray-600 body-font">
+                <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2"
+                            class="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                        <span class="ml-3 text-xl">Quick</span>
+                    </a>
+                    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                        <a class="mr-5 hover:text-gray-900" href="{{ route('search') . '?category=kampus' }}">Kampus</a>
+                        <a class="mr-5 hover:text-gray-900" href="{{ route('search') . '?category=jurusan' }}">Jurusan</a>
+                        <a class="mr-5 hover:text-gray-900" href="{{ route('search') . '?category=karir' }}">Karir</a>
+                        <a class="mr-5 hover:text-gray-900" href="{{ route('search') . '?category=mitra' }}">Mitra</a>
+                    </nav>
+                    @guest
+                    <ul class="flex gap-x-2">
+                        <li><a class="px-4 py-3 border" href="/register">Daftar</a></li>
+                        <li><a class="px-4 py-3 bg-indigo-500" href="/login">Masuk</a></li>
+                    </ul>
+                    @endguest
+                    @auth
+                    <ul class="flex gap-x-2">
+                        <li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <a class="px-4 py-3 border" href="" x-data
+                                    @click="$event.preventDefault(); document.getElementById('logout-form').submit()">
+                                    Keluar
+                                </a>
+                            </form>
+                        </li>
+                    </ul>
+                    @endauth
+                </div>
+            </header>
         </header>
         <main>
 
             @yield('body')
-            <div class="w-10/12 mx-auto py-4 bg-gray-300">
-                <ul class="px-20 flex justify-between">
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-school fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Kampus
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-graduation-cap fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Link
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-briefcase fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Karir
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-award fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Beasiswa
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-diagnoses fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Uji Potensi
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="far fa-handshake fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Mitra
-                    </a></li>
-                    <li><a class="flex flex-col place-items-center" href="">
-                        <i class="fas fa-book-reader fa-2x p-5 bg-blue-500 rounded-md"></i>
-                        Belajar
-                    </a></li>
-                </ul>
-            </div>
+            
 
         </main>
-        <footer>
-            <div class="bg-linear-pink-invert py-12 px-4">
-                <div tabindex="0" aria-label="footer"
-                    class="focus:outline-none mx-auto container flex flex-col items-center justify-center">
-                    <div tabindex="0" aria-label="prodify logo" role="img">
-                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg1.svg"
-                            alt="logo">
+        <footer class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="flex flex-wrap md:text-left text-center order-first">
+                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                        <nav class="list-none mb-10">
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                            </li>
+                        </nav>
                     </div>
-                    <div class="text-black flex flex-col md:items-center f-f-l pt-3">
-                        <h1 tabindex="0" class="focus:outline-none text-2xl font-black">Daftar. Pilih. Selamat.</h1>
-                        <div class="md:flex items-center mt-5 md:mt-10 text-base text-color f-f-l">
-                            <h2 class=" md:mr-6 pb-4 md:py-0 cursor-pointer"><a
-                                    class="focus:outline-none focus:underline hover:text-gray-700"
-                                    href="javascript:void(0)">Download Now</a> </h2>
-                            <h2 class="cursor-pointer"><a class="focus:outline-none focus:underline hover:text-gray-700"
-                                    href="javascript:void(0)">License</a> </h2>
-                        </div>
-                        <div class="my-6 text-base text-color f-f-l">
-                            <ul class="md:flex items-center">
-                                <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">About </a></li>
-                                <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">Features </a>
-                                </li>
-                                <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">Pricing </a></li>
-                                <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">Careers </a></li>
-                                <li class="md:mr-6 cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">Help </a></li>
-                                <li class="cursor-pointer pt-4 lg:py-0"><a href="javascript:void(0)"
-                                        class="focus:outline-none focus:underline hover:text-gray-500">Privacy Policy
-                                    </a></li>
-                            </ul>
-                        </div>
-                        <div class="text-sm text-color mb-10 f-f-l">
-                            <p tabindex="0" class="focus:outline-none">© 2022 Quick. All rights reserved</p>
-                        </div>
+                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                        <nav class="list-none mb-10">
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                            </li>
+                        </nav>
                     </div>
-                    <div class="w-9/12 h-0.5 bg-gray-100 rounded-full"></div>
-                    <div class="flex justify-between items-center pt-12">
-                        <a href="javascript:void(0)"
-                            class="hover:shadow-lg mr-4 focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
-                            aria-label="download on the app store">
-                            <div class="">
-                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg2.svg"
-                                    alt="download on the app store">
+                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                        <nav class="list-none mb-10">
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                            </li>
+                            <li>
+                                <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                            </li>
+                        </nav>
+                    </div>
+                    <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+                        <div
+                            class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
+                            <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                                <label for="footer-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
+                                <input type="text" id="footer-field" name="footer-field"
+                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-red-200 focus:border-red-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
+                            <button
+                                class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
+                        </div>
+                        <p class="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
+                            <br class="lg:block hidden">waistcoat green juice
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-gray-100">
+                <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
+                    <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2022 Quick — All rights reserved.
+                    </p>
+                    <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                        <a class="text-gray-500">
+                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                            </svg>
                         </a>
-                        <button
-                            class="hover:shadow-lg focus:outline-none rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
-                            aria-label="get it on google play"></a>
-                            <div>
-                                <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/simple_center_aligned_with_logo-svg3.svg"
-                                    alt="get it on google play">
-                            </div>
-                        </button>
-                    </div>
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                class="w-5 h-5" viewBox="0 0 24 24">
+                                <path
+                                    d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                                </path>
+                            </svg>
+                        </a>
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                            </svg>
+                        </a>
+                        <a class="ml-3 text-gray-500">
+                            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path stroke="none"
+                                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
+                                </path>
+                                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                            </svg>
+                        </a>
+                    </span>
                 </div>
             </div>
         </footer>
