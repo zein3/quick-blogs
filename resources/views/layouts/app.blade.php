ec391f0df5d1aa3dfb2a3de5c2f4930ffe23b985
Fakhirul Akmal <fakhirula27@gmail.com>
2022-01-24 19:31:55 +0700

    Edit Index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -133,16 +133,14 @@
                         </nav>
                     </div>
                     <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+                        <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">NEWSLETTER</h2>
                         <div class="flex flex-wrap justify-center items-end md:justify-start">
                             <div class="relative w-40 sm:w-auto lg:mr-0 sm:mr-4 mr-2">
                                 <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
-                            <button class="md:mt-1 lg:mt-2 flex-shrink-0 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Button</button>
+                            <button class="md:mt-1 lg:mt-2 flex-shrink-0 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Subscribe</button>
                         </div>
-                        <p class="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
-                            <br class="lg:block hidden">waistcoat green juice
-                        </p>
+                        <p class="text-gray-500 text-sm mt-2 md:text-left text-center">Dapatkan informasi terupdate dari Quick</p>
                     </div>
                 </div>
             </div>
